50b426daa417199d3c892527cf527a41e3f44af8
charlotteglen <164542857+charlotteglen@users.noreply.github.com>
2024-11-07 14:46:00 +0000

    Add files via upload

---
+++ Sprint 3/Code/manageteams.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+include("connect.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instructor's Homepage</title>
+    <link rel="stylesheet" href="teacher_style.css">
+    <script src="script.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+    <!--font-->
+</head>
+
+<body>
+     <!--welcome banner -->
+     <div id = "welcome">
+      <h2 id="welcome">
+       Welcome <?php 
+       if(isset($_SESSION['userName'])){
+        $userName=$_SESSION['userName'];
+        $query=mysqli_query($conn, "SELECT users.* FROM `users` WHERE users.userName='$userName'");
+        while($row=mysqli_fetch_array($query)){
+            echo $row['firstName'] . '!';
+        }
+       }
+       ?> </h2>
+
+      <h3 style=font-size:30px>Instructor Dashboard</h3>
+
+      <div class = "changepage">
+      <form action="teacherpage.php" method="get">
+        <button class="button-3" role="button">View Teams</button>
+      </form>
+      </div>
+      </div>
+
+      <!--content wrapper to display team assignment and table side by side-->
+      <div class ="content-wrapper">
+
+      <div class="infotable" id="infoTable">
+      <h2 style = padding:12px;text-align:center>Student Information</h2>
+
+        <table id ="students" class="teamTable">
+            <thead>
+                <tr>
+                    <th>Team</th>
+                    <th>Name</th>
+                    <th>Student ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $allUsersQuery = mysqli_query($conn, "SELECT * FROM `users` WHERE `role` = 'student'");
+                
+                while($user = mysqli_fetch_array($allUsersQuery)){
+                    echo "<tr>";
+                    echo "<td>" . $user['team'] . "</td>";
+                    echo "<td>" . $user['firstName'] . " " . $user['lastName'] . '</td>';
+                    echo "<td>" . $user['studentid'] . '</td>';
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+        </div>
+
+        <div class="team-creation" id="teamCreation">
+        <h2 style=text-align:center;padding:10px>Create or Change Teams</h2>
+
+        <div class="assign-method">
+        <form method="post" action="register.php">
+        <p style=font-weight:bold>Assign Manually</p>
+
+        <div class="input-group">
+            <label for="fname">Enter Student's ID:</label>
+            <i class="fas fa-user"></i>
+           <input type="text" name="studentid" id="studentid" placeholder="Student Id" required>
+        </div>
+
+          <div class="input-group">
+            <label for="userName">Enter the Student's Team Number:</label>
+              <i class="fas fa-user"></i>
+              <input type="number" step="1" name="team" id="team" placeholder="User Team" required min="1">
+          </div>
+
+          <button value="Change" name="change">Assign Student</button>
+
+        </form>
+
+      </div>
+
+      <div class="assign-method">
+        <p style= font-weight:bold>Upload class CSV file</p>
+        <form action="#">
+          <input type="file" id="myFile" name="filename">
+          <input type="submit">
+        </form>
+      </div>
+
+      <div class="assign-method">
+        <p style="font-weight:bold">Export Information to CSV File</p>
+            <a href="export_csv.php">
+              <button>Export</button>
+            </a>
+      </div>
+
+      </div>
+
+    </div>
+
+    <div class="logout">
+      <form action="logout.php" method="get">
+        <button class="button-3" role="button">Logout</button>
+      </form>
+    </div>
+
+      <div class ="settings">
+        <form action = # method ="get">
+          <button class="button-3" role= "button" value = "Settings">Settings</button>
+        </form>
+      </div>
+
+</body>
---
+++ Sprint 3/Code/teacherpage.php
@@ -0,0 +1,109 @@
+<?php
+session_start();
+include("connect.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instructor's Homepage</title>
+    <link rel="stylesheet" href="teacher_style.css">
+    <script src="script.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+</head>
+
+<body>
+  <!--welcome banner -->
+    <div id = "welcome">
+      <h2 id="welcome">
+       Welcome <?php 
+       if(isset($_SESSION['userName'])){
+        $userName=$_SESSION['userName'];
+        $query=mysqli_query($conn, "SELECT users.* FROM `users` WHERE users.userName='$userName'");
+        while($row=mysqli_fetch_array($query)){
+            echo $row['firstName'] . '!';
+        }
+       }
+       ?> </h2>
+
+      <h3 style=font-size:30px>Instructor Dashboard</h3>
+
+      <div class = "change-page">
+      <form action="manageteams.php" method="get">
+        <button class="button-3" role="button">Manage Teams</button>
+      </form>
+      </div>
+
+      <div class = "change-page">
+      <form action="teacherdetail.php" method="get">
+        <button class="button-3" role="button">Detailed View</button>
+      </form>
+      </div>
+      
+      </div>
+
+        <div id="table-container">
+        <?php
+
+        $teamQuery = mysqli_query($conn, "SELECT DISTINCT team FROM `users` WHERE `role` = 'student'");
+        while ($teamRow = mysqli_fetch_array($teamQuery)) {
+          $teamNumber = $teamRow['team'];
+
+             echo "<table class='teamTable' id='team-$teamNumber'>
+                    
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Student ID</th>
+                            <th>Cooperation Dimension</th>
+                            <th>Conceptual Contribution</th>
+                            <th>Practical Contribution</th>
+                            <th>Work Ethic</th>
+                            <th>Team</th>
+                            <th>Average</th>
+                            <th>Peers who responded</th>
+                        </tr>
+                    </thead>
+                    <tbody>";
+            
+            $teamUsersQuery = mysqli_query($conn, "SELECT * FROM `users` WHERE `role` = 'student' AND `team` = '$teamNumber'");
+            while($user = mysqli_fetch_array($teamUsersQuery)){
+                echo "<tr>";
+                echo "<td>" . $user['firstName'] . " " . $user['lastName'] . "</td>";
+                echo "<td>" . $user['studentid'] . "</td>";
+                echo "<td>" . $user['evalu'] . "</td>"; // Display Cooperation Dimension
+                echo "<td>" . $user['evalu1'] . "</td>"; // Display Conceptual Contribution
+                echo "<td>" . $user['evalu2'] . "</td>"; // Display Practical Contribution
+                echo "<td>" . $user['evalu3'] . "</td>"; // Display Work Ethic
+                echo "<td>" . $user["team"] . "</td>";
+                echo "<td>" . $user['totalEvalu'] . "</td>";
+                echo "<td>" . $user['numComment'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</tbody></table>";
+        }
+        ?>
+    
+    </div>
+    
+    </div>
+
+    <div class="logout">
+      <form action="logout.php" method="get">
+        <button class="button-3" role="button">Logout</button>
+      </form>
+    </div>
+
+    <div class ="settings"> <!--this is added due to possibly being needed later and makes page look nicer? can remove easily-->
+      <form action = # method ="get">
+        <button class="button-3" role= "button" value = "Settings">Settings</button>
+      </form>
+    </div>
+
+
+</body>
+</html>

1d4d121fb919aee43583da2effce1576f9d96dba
Ramiro <djuarezramiro@gmail.com>
2024-11-24 18:17:13 -0500

    restored student evaluation style

--- Sprint 4/Code/studentevalu.php
+++ Sprint 4/Code/studentevalu.php
@@ -14,6 +14,7 @@ include("connect.php");
         <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'></title>
         <!-- link reference for css -->
         <link rel="stylesheet" href = "homepage.css">
+        <link rel="stylesheet" href = "studentevalu.css">
         <style>
         table {
             width: 80%;
@@ -49,7 +50,7 @@ include("connect.php");
             <a href = "studenttask.php"> <i class = "fas fa-tasks"></i> Task </a>
             <a href = "logout.php" class = "right"> Logout </a>
         </div>
-    <div style="text-align:center; padding:15%;">
+    <div style="text-align:center; padding:15%; padding-bottom:10%">
       <p style="font-size:50px; font-weight:bold;">
        Welcome student 
        <?php 
@@ -105,8 +106,10 @@ include("connect.php");
       ?>
     </div>
 
+        <!----------------------- Evaluation Section -------------------------->
+
     <?php if(!empty($studentGroup)) { ?>
-    <div class="container" id="evaluation">
+    <div id="evaluation">
         <h1 class="form-title">Student Evaluation Section:</h1>
         <form method="post" action="register.php">
 
@@ -121,73 +124,100 @@ include("connect.php");
         </div>
 
         <!-- Cooperation dimension evaluation input-->
-        <div class="input-group">
+        <div class="evaluation-section" id="cooperation-section">
             <p>Choose a rating for Cooperation dimension:</p>
-            <select name="rating" id="rating">
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
             <br>
-            <label for="comment">Enter the Cooperation Dimension evaluation:</label>
+            <p>This assessment dimension comprises actively participating in meetings; Communicating within the group;
+                <br> Cooperating within the group; Assisting team-mates when needed; Volunteering for tasks.
+            </p>
+            <br>
+            <div class="card">
+                <input
+                    class="rating rating--nojs"
+                    max="5"
+                    step="1"
+                    type="range"
+                    value="0"
+                    name="cooperation">
+            </div>
+            <br>
+            <label for="comment">Comment on this teammate's cooperation (optional):</label>
             <br>
             <textarea name="comment" id="comment" rows="4" placeholder="Write your comment here..."></textarea>
         </div>
 
         <!-- Conceptual Contribution evaluation input-->
-        <div class="input-group">
-            <p>Choose a rating for Conceptual Contribution:</p>
-            <select name="rating1" id="rating1">
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <br>
-            <label for="comment1">Enter the Conceptual Contribution evaluation:</label>
-            <br>
-            <textarea name="comment1" id="comment1" rows="4" placeholder="Write your comment here..."></textarea>
+        <div class="evaluation-section" id="contribution-section">
+        <p>Choose a rating for Conceptual Contribution:</p>
+        <br>
+        <p> This assessment dimension comprises Researching and gathering information; Quality of individual contribution; 
+            <br>Suggesting ideas; Tying ideas together; Identifying difficulties; Identifying effective approaches.
+        </p>
+        <br>
+        <div class="card">
+            <input
+                class="rating rating--nojs"
+                max="5"
+                step="1"
+                type="range"
+                value="0"
+                name="contribution">
         </div>
+        <br>
+        <label for="comment1">Comment on this teammate's conceptual contribution (optional):</label>
+        <br>
+        <textarea name="comment1" id="comment1" rows="4" placeholder="Write your comment here..."></textarea>
+    </div>
 
         <!-- Practical Contribution evaluation input-->
-        <div class="input-group">
-            <p>Choose a rating for Practical Contribution:</p>
-            <select name="rating2" id="rating2">
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <br>
-            <label for="comment2">Enter the Practical Contribution evaluation:</label>
-            <br>
-            <textarea name="comment2" id="comment2" rows="4" placeholder="Write your comment here..."></textarea>
+        <div class="evaluation-section" id="practical-section">
+        <p>Choose a rating for Practical Contribution:</p>
+        <br>
+        <p>
+            This assessment dimension comprises Writing of the report(s); Reviewing others’ report(s) or section(s);
+            <br> Providing constructive feedback on the report(s) or the presentation; Contributing to the organization of the work; 
+            <br>Contributing to the preparation of presentation(s) (if appropriate).
+        </p>
+        <br>
+        <div class="card">
+            <input
+                class="rating rating--nojs"
+                max="5"
+                step="1"
+                type="range"
+                value="0"
+                name="practical">
         </div>
+        <br>
+        <label for="comment2">Comment on this teammate's practical contribution (optional):</label>
+        <br>
+        <textarea name="comment2" id="comment2" rows="4" placeholder="Write your comment here..."></textarea>
+    </div>
 
         <!-- Work Ethic evaluation input-->
-        <div class="input-group">
-            <p>Choose a rating for Work Ethic:</p>
-            <select name="rating3" id="rating3">
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <br>
-            <label for="comment3">Enter the Work Ethic evaluation:</label>
-            <br>
-            <textarea name="comment3" id="comment3" rows="4" placeholder="Write your comment here..."></textarea>
+        <div class="evaluation-section" id="workethic-section">
+        <p>Choose a rating for Work Ethic:</p>
+        <br>
+        <p>
+            This assessment dimension comprises Displaying a positive attitude; Respecting team-mates; 
+            <br>Respecting commitments; Respecting deadlines; Respecting team-mates’ ideas.
+        </p>
+        <br>
+        <div class="card">
+            <input
+                class="rating rating--nojs"
+                max="5"
+                step="1"
+                type="range"
+                value="0"
+                name="workethic">
         </div>
-        <input type="submit" class="btn" value="Submit Evaluation" name="evalu">
+        <br>
+        <label for="comment3">Comment on this teammate's wrok ethic (optional):</label>
+        <br>
+        <textarea name="comment3" id="comment3" rows="4" placeholder="Write your comment here..."></textarea>
+    </div>
+        <input type="submit" class="btn" value="Submit Evaluation" name="evalu" onclick="alert('Form is being submitted!');">
         </form>
     </div>
     <?php } // End if for evaluation section ?>

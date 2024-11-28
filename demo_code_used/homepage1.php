3ea8e6370a57ec8d1074646b844064b77502d542
TimmyTwoTimes <vuminhtien2110@gmail.com>
2024-11-28 15:43:13 -0500

    work flow triggers

---
+++ demo_code_used/homepage1.php
@@ -0,0 +1,60 @@
+<!-- dashboard homepage -->
+<!DOCTYPE html>
+ <html lang = "en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Peer Assessment </title> 
+        <!-- icons -->
+        <link rel="stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <!-- font -->
+        <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'></title>
+        <!-- link reference for css -->
+        <link rel="stylesheet" href = "homepage.css">
+    </head>
+
+    <body>
+        <!-- header -->
+        <div class = "bg-img"></div>
+        <div class = "bg"></div>
+        <div class = "header">
+            <a href = "homepage.html">
+                <i class = "fas fa-user-circle"></i>
+            </a>
+            <h1> Teacher Peer Assessment </h1>
+        </div>
+
+        <!-- navigation bar -->
+        <div class = "navbar">
+            <a href = "homepage1.php"> <i class = "fas fa-home"></i> Home </a>
+            <a href = "teacherpage.php"> <i class = "fas fa-users"></i> Team </a>
+            <a href = "teacherdetail.php"> <i class = "fas fa-users"></i> Detailed View </a>
+            <a href = "assign.php"> <i class = "fas fa-pencil-square"></i> Group Setting </a>
+            <a href = "teachertask.php"> <i class = "fas fa-tasks"></i> Task </a>
+            <a href = "noti.php"> <i class = "fas fa-bell"></i> Notification </a>
+            <a href = "logout.php" class = "right"> Logout </a>
+        </div>
+
+        <div class = "home">
+            <!-- insert welcome user -->
+            
+            <!-- instructions -->
+            <div class = "instructions">
+                <h2> Peer Assessment </h2> <br> 
+                <p> 
+                    The Peer Assessment Application enables students to annonymously evaluate their teammates accross multiple dimensions, such as cooperation, conceptual contribution, practical contribution and work ethic. 
+                    By providing a structured platform for feedback, the application supported fairness, accountability, and personal growth. Instructors can also use this application to monitor team dynamics and individual contribution and performance to help them make informed assessments.
+                </p> <br>
+
+                <h2> Dashboard </h2> <br>
+                <p>
+                    Home: Home page <br>
+                    Team: View assigned team and teammates <br>
+                    Peer Assessment: Evaluate and assess team members <br>
+                    Task: To-do list or checklist to see upcoming assignments, meetings or labs. <br>
+                </p>
+            </div>
+        </div>
+
+    </body>
+ </html>
